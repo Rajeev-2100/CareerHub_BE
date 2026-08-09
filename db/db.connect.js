f05961060a03f502844e59286336751d6371e7b7
Rajeev Rawat <rajeevrawat2100@email.com>
2026-08-09 07:30:48 +0530

    fixing the code in backend

--- db/db.connect.js
+++ db/db.connect.js
@@ -4,10 +4,14 @@ require("dotenv").config();
 const MONGO_Uri = process.env.MONGO_URL;
 
 const initializationDatabase = async () => {
-  await mongoose
-    .connect(MONGO_Uri)
-    .then(() => console.log("Connect Database Successfully"))
-    .catch((error) => console.log("Failed to connect Database"));
+  try {
+    await mongoose.connect(MONGO_Uri);
+    console.log("Connect Database Successfully");
+  } catch (error) {
+    console.error("Failed to connect Database:", error);
+    // Throwing error so that if it fails, we know about it instead of silently buffering requests forever
+    throw error;
+  }
 };
 
-module.exports = { initializationDatabase }
+module.exports = { initializationDatabase };
